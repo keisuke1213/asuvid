15acba2eba50b1d4792f6924b14c5cc8e979260c
keisuke1213 <mayakufu2694@outlook.jp>
2024-08-08 23:40:04 +0900

    mui-iconをインストール

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,16 +1,9 @@
 "use client";
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
-import { Drawer, List, ListItemButton, ListItemText, Divider} from '@mui/material';
+import { Drawer, List, ListItemButton, ListItemText, Divider,AppBar,Box,Toolbar,styled,alpha,IconButton,Typography, InputBase } from '@mui/material';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
