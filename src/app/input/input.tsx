dc09083700cac5078e315ffb40a5798fe78a5686
keisuke1213 <mayakufu2694@outlook.jp>
2024-08-09 00:01:53 +0900

    不要なものを削除

--- src/app/input/input.tsx
+++ src/app/input/input.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from 'react';
 import axios from 'axios';
-import { Bar } from 'react-chartjs-2';
 import { Container, TextField, Button, Box, Typography, Paper } from '@mui/material';
 
 export const Input = () => {
